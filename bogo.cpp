0f46dfeccace33a7ca7de4debc096f3438790f39
apl1341 <apl1341@rit.edu>
2019-12-08 21:19:12 -0500

    added speed calculations

--- bogo.cpp
+++ bogo.cpp
@@ -14,14 +14,19 @@ int main(int c, char** argc){
 	Sorter sorter(num);
 	sorter.print();
 	time_t start = time(NULL);
+	unsigned long count = 1;
 	while(!sorter.isValid()){
 		sorter.randomize();
+		cout<<count<<": ";
 		sorter.print();
+		count++;
 	}
 	time_t taken = time(NULL) - start;
 	int min = taken/60;
 	int sec = taken%60;
-	cout<<"Ideal configuration reached in "<<min<<"m "<<sec<<"s : "<<endl;
+	cout<<"Ideal configuration reached in "<<min<<"m "<<sec<<"s : through "<<count<<" cycles"<<endl;
 	sorter.print();
+	int speed_avg = count/taken;
+	cout<<"Average processing speed was "<<speed_avg<<" per second."<<endl;
 	return 0;
 }
